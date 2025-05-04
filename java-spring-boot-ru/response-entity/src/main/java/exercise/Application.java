33671ddb352e213a50e79d1678a299768134df1b
@hexlet/cli <support@hexlet.io>
2025-05-04 10:46:46 +0700

    submit java-spring-boot-ru/response-entity

--- java-spring-boot-ru/response-entity/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/response-entity/src/main/java/exercise/Application.java
@@ -46,6 +46,7 @@ public class Application {
         var post = posts.stream()
                 .filter(p -> p.getId().equals(id))
                 .findFirst();
+
         return ResponseEntity.of(post);
     }
 
@@ -64,13 +65,15 @@ public class Application {
         var maybePost = posts.stream()
                 .filter(p -> p.getId().equals(id))
                 .findFirst();
+
         if (maybePost.isPresent()) {
             var post = maybePost.get();
             post.setId(data.getId());
             post.setTitle(data.getTitle());
             post.setBody(data.getBody());
         }
-        return ResponseEntity.of(maybePost);
+
+        return ResponseEntity.ok(data);
     }
     // END
 
